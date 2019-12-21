d18bcf2ed3c094fd7e910bb1d265e54d01497372
David Wu <wudawei01@gmail.com>
2019-12-21 11:01:46 +0000

    Add constant G and calcForceExertedBy to Body

--- proj0/Body.java
+++ proj0/Body.java
@@ -6,6 +6,8 @@ public class Body {
     public double mass;
     public String imgFileName;
 
+    static final double G = 6.67e-11;
+
     public Body(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
         yyPos = yP;
@@ -21,5 +23,10 @@ public class Body {
 
     public double calcDistance(Body b) {
         return Math.sqrt(Math.pow(xxPos - b.xxPos, 2) + Math.pow(yyPos - b.yyPos, 2));
+    }
+
+    public double calcForceExertedBy(Body b) {
+        double distance = calcDistance(b);
+        return G * mass * b.mass / (distance * distance);
     }
 }
