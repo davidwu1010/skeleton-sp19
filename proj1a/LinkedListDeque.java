a6bc96172a62617a8fce1feb858403a3e4e8e1b0
David Wu <wudawei01@gmail.com>
2019-12-25 11:54:30 +0000

    Implement printDeque

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -16,24 +16,42 @@ public class LinkedListDeque<T> {
     }
 
     public LinkedListDeque(LinkedListDeque<T> other) {}
+
     public void addFirst(T item) {}
+
     public void addLast(T item) {}
+
     public boolean isEmpty() {
-        return false;
+        return _size == 0;
     }
+
     public int size() {
-        return 0;
+        return _size;
     }
-    public void printDeque() {}
+
+    public void printDeque() {
+        for (Node<T> p = head.next; p != head; p = p.next) {
+            if (p == head.next) {
+                System.out.printf("%s", p.item);
+            } else {
+                System.out.printf(" %s", p.item);
+            }
+        }
+        System.out.println();
+    }
+
     public T removeFirst() {
         return null;
     }
+
     public T removeLast() {
         return null;
     }
+
     public T get(int index) {
         return null;
     }
+
     public T getRecursive(int index) {
         return null;
     }
