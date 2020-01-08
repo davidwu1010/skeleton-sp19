e75dc641c28b184f4149da6ac36784ddc7ff22df
David Wu <wudawei01@gmail.com>
2020-01-08 22:12:38 +0000

    Implement ArrayRingBuffer and write tests

--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -7,8 +7,45 @@ import static org.junit.Assert.*;
  */
 
 public class TestArrayRingBuffer {
+    @Test(expected = RuntimeException.class)
+    public void testOverflow() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(4);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+        arb.enqueue(5);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testUnderflow1() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(4);
+        arb.dequeue();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testUnderflow2() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(4);
+        arb.peek();
+    }
+
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void generalTest() {
+        ArrayRingBuffer arb = new ArrayRingBuffer(4);
+        assertEquals(4, arb.capacity());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+        assertEquals(4, arb.fillCount());
+        assertEquals(1, arb.peek());
+        assertEquals(1, arb.dequeue());
+        arb.enqueue(5);
+        assertEquals(2, arb.dequeue());
+        assertEquals(3, arb.dequeue());
+        assertEquals(4, arb.dequeue());
+        assertEquals(5, arb.dequeue());
+        arb.enqueue(6);
+        assertEquals(6, arb.dequeue());
     }
 }
